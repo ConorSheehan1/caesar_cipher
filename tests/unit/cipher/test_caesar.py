6c22d81277fff1ad9fc781ab31e6f48419cb171d
Conor Sheehan <conor.sheehan.dev@gmail.com>
2019-12-29 20:55:08 +0000

    add more unit tests.

--- tests/unit/cipher/test_caesar.py
+++ tests/unit/cipher/test_caesar.py
@@ -9,6 +9,13 @@ class TestCaesar(unittest.TestCase):
     it should preserve capitalization. e.g. AsDf, 2 -> CuFh.
     """
 
+    def __init__(self, methodName="runTest"):
+        unittest.TestCase.__init__(self, methodName)
+        self.arg_error_msg = (
+            "text must be a string (letters and symbols in quotes) e.g. 'Hi'\n"
+            "increment must be an integer (whole number) e.g. 1"
+        )
+
     def test_encrypt_case_sensitive(self):
         assert encrypt("Hello World!", 1) == "Ifmmp Xpsme!"
 
@@ -16,9 +23,29 @@ class TestCaesar(unittest.TestCase):
         """Using a negative value for the offset should undo the encryption"""
         assert encrypt("Ifmmp Xpsme!", -1) == "Hello World!"
 
+    def test_circular_offset(self):
+        """uses circular list, so 1 is the same as 27 (26 + 1)"""
+        assert encrypt("AsDf", 1) == encrypt("AsDf", 27)
+
+    def test_negative_circular_offset(self):
+        """uses circular list, so -1 is the same as 25 (26 - 1)"""
+        assert encrypt("AsDf", -1) == encrypt("AsDf", 25)
+
     def test_no_change(self):
         """Using 0 as the offset value should not change the result at all"""
         assert encrypt("asdf", 0) == "asdf"
+
+    def test_numbers(self):
+        """numbers should not change, since they are not in [a-zA-Z]"""
+        assert encrypt("1234", 1) == "1234"
+
+    def test_invalid_offset(self):
+        """args must be (str, int), not (str, str)"""
+        assert encrypt("asdf", "asdf") == self.arg_error_msg
+
+    def test_invalid_text(self):
+        """args must be (str, int), not (int, int)"""
+        assert encrypt(1, 1) == self.arg_error_msg
 
 
 if __name__ == "__main__":
