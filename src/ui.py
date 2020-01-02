409be82fe8c93a83b9b55d8d0b8fcafa8e213a12
Conor Sheehan <conor.sheehan.dev@gmail.com>
2020-01-02 18:39:49 +0000

    refactor ui, show all errors, return original text on increment error.

--- src/ui.py
+++ src/ui.py
@@ -9,26 +9,20 @@ class App(object):
         self.root.geometry("500x200")
         self.root.wm_title("Ceasar Cipher")
 
-        self.label = Label(self.root, text="Enter a string")
-        self.label.pack()
+        Label(self.root, text="Enter a string to be encrypted").pack()
+        self.entry_text = StringVar()
+        Entry(self.root, textvariable=self.entry_text, width="400").pack()
 
-        self.entrytext = StringVar()
-        Entry(self.root, textvariable=self.entrytext, width="400").pack()
+        Label(self.root, text="Enter a number to shift the letters").pack()
+        self.entry_number = StringVar()
+        Entry(self.root, textvariable=self.entry_number, width="400").pack()
 
-        self.label2 = Label(
-            self.root, text="Enter number of letters to shift in the alphabet"
-        )
-        self.label2.pack()
+        self.button_text = StringVar()
+        self.button_text.set("Calculate")
+        Button(self.root, textvariable=self.button_text, command=self.on_click).pack()
 
-        self.entrynumber = StringVar()
-        Entry(self.root, textvariable=self.entrynumber, width="400").pack()
-
-        self.buttontext = StringVar()
-        self.buttontext.set("Calculate")
-        Button(self.root, textvariable=self.buttontext, command=self.clicked).pack()
-
-        self.error = Label(self.root, text="")
-        self.error.pack()
+        self.error_label = Label(self.root, text="", foreground="red")
+        self.error_label.pack()
 
         self.result = Text(self.root, height=10, width="400")
         self.result.configure(state="disabled")
@@ -36,30 +30,30 @@ class App(object):
 
         self.root.mainloop()
 
-    def clicked(self):
+    def append_error(self, error_text):
+        self.error_label.configure(
+            text=f"{self.error_label.cget('text')} {error_text}\n"
+        )
+
+    def on_click(self):
         # allow text to be inserted
         self.result.configure(state="normal")
-        # flush old text
+        # flush old result and errors
         self.result.delete(1.0, END)
+        self.error_label.configure(text="")
 
-        user_string = self.entrytext.get()
+        user_string = self.entry_text.get()
         try:
-            user_increment = int(self.entrynumber.get())
+            user_increment = int(self.entry_number.get())
         except ValueError:
-            error_message = "please insert a number in the second text box"
-            print(error_message)
-            self.error.configure(text=error_message)
-            # break out of function to ensure no value error converting empty string to int
-            return False
+            self.append_error("please insert a number in the second input")
+            user_increment = 0
 
         result = encrypt(user_string, user_increment)
         if result == "":
-            # result is now an error message
-            error_message = "please insert a string in the first text box"
-            print(error_message)
-            self.error.configure(text=error_message)
+            self.append_error("please insert a string in the first input")
 
-        # return values in disabled text field to allow users to copy text to clipboard
+        # disable text field so text can be copied, but not modified
         self.result.insert(1.0, result)
         self.result.configure(state="disabled")
         return True
